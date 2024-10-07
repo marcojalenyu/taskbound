0580a29a281e355e5b524d5ff1bab87982b707f0
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2024-10-07 17:45:06 +0800

    feat: Created Collectible class

---
+++ app/src/main/java/com/mobdeve/s17/taskbound/MyCollectiblesData.java
@@ -0,0 +1,41 @@
+package com.mobdeve.s17.taskbound;
+
+public class MyCollectiblesData {
+    private final String collectibleID;
+    private final String collectibleName;
+    private Rarity collectiblesRarity;
+    private boolean isObtained;
+    private final Integer collectibleImage;
+
+    public MyCollectiblesData(String collectibleID, String collectibleName, String rarity, Integer collectibleImage) {
+        this.collectibleID = collectibleID;
+        this.collectibleName = collectibleName;
+        this.collectiblesRarity = Rarity.valueOf(rarity);
+        this.isObtained = false;
+        this.collectibleImage = collectibleImage;
+    }
+
+    public Integer getCollectibleImage() {
+        return collectibleImage;
+    }
+
+    public boolean isObtained() {
+        return isObtained;
+    }
+
+    public void setObtained(boolean obtained) {
+        this.isObtained = obtained;
+    }
+
+    public String getCollectibleName() {
+        return collectibleName;
+    }
+
+    public Rarity getCollectiblesRarity() {
+        return collectiblesRarity;
+    }
+
+    public String getCollectibleID() {
+        return collectibleID;
+    }
+}
