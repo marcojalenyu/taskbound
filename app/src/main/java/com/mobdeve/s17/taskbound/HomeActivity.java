963b5c7d7b4ba4ee2e91168fafa5a97ad5de591b
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2024-10-14 22:38:43 +0800

    fix: Redid the main layout

--- app/src/main/java/com/mobdeve/s17/taskbound/HomeActivity.java
+++ app/src/main/java/com/mobdeve/s17/taskbound/HomeActivity.java
@@ -1,16 +1,11 @@
 package com.mobdeve.s17.taskbound;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,8 +14,6 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -28,8 +21,8 @@ public class HomeActivity extends AppCompatActivity {
     FloatingActionButton shopBtn;
     FloatingActionButton addTaskButton;
     ArrayList<MyCollectiblesData> collectiblesList;
-    TextView username;
-    TextView coinAmount;
+    TextView tvUsername;
+    TextView tvCoinAmount;
     private UserSession userSession;
 
     @Override
@@ -46,12 +39,12 @@ public class HomeActivity extends AppCompatActivity {
         this.userSession = UserSession.getInstance();
         this.collectiblesList = userSession.getCurrentUser().getCollectiblesList();
 
-        this.username = findViewById(R.id.username);
-        this.coinAmount = findViewById(R.id.coinAmount);
+        this.tvUsername = findViewById(R.id.tvUsername);
+        this.tvCoinAmount = findViewById(R.id.tvCoinAmount);
 
-        this.username.setText(userSession.getCurrentUser().getUserName());
+        this.tvUsername.setText(userSession.getCurrentUser().getUserName());
         //this is just set on start, but it will be updated onResume
-        this.coinAmount.setText(String.valueOf(userSession.getCurrentUser().getCoins()));
+        this.tvCoinAmount.setText(String.valueOf(userSession.getCurrentUser().getCoins()));
 
 
         this.collectiblesBtn = findViewById(R.id.collectiblesBtn);
@@ -88,7 +81,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         // Update the collectibles list and coin amount when the activity resumes
-        this.coinAmount.setText(String.valueOf(userSession.getCurrentUser().getCoins()));
+        this.tvCoinAmount.setText(String.valueOf(userSession.getCurrentUser().getCoins()));
     }
 
     public void btnClickedLogout(View v) {
