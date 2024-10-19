1f3f9ccab05d806b1527115aa4957e30fab0b0d8
marcojalenyu <112309140+marcojalenyu@users.noreply.github.com>
2024-10-19 11:05:01 +0800

    ui: Removed bg of Sort

--- app/src/main/java/com/mobdeve/s17/taskbound/TaskBoundDBHelper.java
+++ app/src/main/java/com/mobdeve/s17/taskbound/TaskBoundDBHelper.java
@@ -15,7 +15,7 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 
 public class TaskBoundDBHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME = "taskbound7.db";
+    public static final String DATABASE_NAME = "taskbound-local.db";
     public static final int DATABASE_VERSION = 1;
 
     // User table
