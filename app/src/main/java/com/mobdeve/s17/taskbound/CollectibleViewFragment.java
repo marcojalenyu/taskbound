e4934cfe58381e0c87c41c1c5dc18e4c355242d4
Dane Chan <dane_chan@dlsu.edu.ph>
2024-11-21 02:08:33 +0800

    Feat: Added fragment to view collectible

---
+++ app/src/main/java/com/mobdeve/s17/taskbound/CollectibleViewFragment.java
@@ -0,0 +1,122 @@
+package com.mobdeve.s17.taskbound;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import java.text.SimpleDateFormat;
+
+public class CollectibleViewFragment extends DialogFragment {
+    // UI components
+    private Button submitButton, closeButton;
+    private ImageView collectibleImageView;
+    private TextView collectibleNameTextView;
+    // Data components
+    private final Collectible collectible;
+    private User currentUser;
+    private LocalDBManager localDB;
+
+    /**
+     * Constructor for the TaskFragment class.
+     * @param collectible - the collectible to be viewed
+     */
+    public CollectibleViewFragment(Collectible collectible) {
+        this.collectible = collectible;
+    }
+
+    /**
+     * This method is called when the fragment is first created.
+     * @param inflater - LayoutInflater object that can be used to inflate
+     * @param container - the parent view that the fragment's UI should be attached to
+     * @param savedInstanceState - the previous saved state
+     */
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_collectible, container, false);
+        initializeUI(view);
+        initializeData();
+        return view;
+    }
+
+    /**
+     * This method initializes the UI components of the dialog box.
+     */
+    private void initializeUI(View view) {
+        this.collectibleImageView = view.findViewById(R.id.dialog_collectible_image);
+        this.collectibleNameTextView = view.findViewById(R.id.dialog_collectible_name);
+        this.submitButton = view.findViewById(R.id.dialog_close_button);
+    }
+
+    /**
+     * This method initializes the data components of the dialog box.
+     */
+    private void initializeData() {
+        this.currentUser = UserSession.getInstance().getCurrentUser();
+        this.localDB = new LocalDBManager(this.getContext());
+        this.collectibleNameTextView.setText(this.collectible.getCollectibleName());
+        this.collectibleImageView.setImageResource(this.collectible.getCollectibleImage());
+
+        String text = "Set pfp";
+        this.submitButton.setTag(text);
+
+        submitButton.setOnClickListener(this::btnClickedSubmit);
+    }
+
+    /**
+     * This method is called when the submit button is clicked.
+     */
+    public void btnClickedSubmit(View view) {
+        // Get the task information
+//        String name = String.valueOf(etTaskName.getText());
+//        String content = String.valueOf(etTaskDesc.getText());
+//        String deadline = String.valueOf(etDeadline.getText());
+//        try {
+//            localDB.updateTaskInfo(task.getId(), name, content, deadline);
+//            dismiss();
+//            ((HomeActivity) getActivity()).onResume();
+//        } catch (Exception e) {
+//            Log.e("LoginReal", e + "");
+//        }
+    }
+
+    /**
+     * This method is called when the dialog is first created.
+     * It sets the width and height of the dialog box.
+     */
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            Window window = getDialog().getWindow();
+            WindowManager.LayoutParams params = window.getAttributes();
+            params.width = WindowManager.LayoutParams.MATCH_PARENT;
+            params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+            params.gravity = Gravity.CENTER;
+            window.setAttributes(params);
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+    }
+}
