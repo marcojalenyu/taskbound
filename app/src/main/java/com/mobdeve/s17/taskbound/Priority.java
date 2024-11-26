7103db176e4efaa5ce8b30c13e95b80eee8455d3
Dane Chan <dane_chan@dlsu.edu.ph>
2024-11-26 17:06:55 +0800

    Feat: Added priority enum

---
+++ app/src/main/java/com/mobdeve/s17/taskbound/Priority.java
@@ -0,0 +1,11 @@
+package com.mobdeve.s17.taskbound;
+
+/**
+ * Enum class for the different priorities of the tasks
+ */
+public enum Priority {
+    // Default sorting is by date and time created
+    LOW,
+    MEDIUM,
+    HIGH,
+}
