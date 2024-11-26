29ac5f93149aa9f2609517c725610d140d6eb060
Dane Chan <dane_chan@dlsu.edu.ph>
2024-11-26 23:34:04 +0800

    Fix: Made sortType enum

---
+++ app/src/main/java/com/mobdeve/s17/taskbound/SortOrder.java
@@ -0,0 +1,11 @@
+package com.mobdeve.s17.taskbound;
+
+/**
+ * Enum class for the different sorting types of the tasks
+ */
+public enum SortOrder {
+    // Default sorting is by date and time created
+    DEFAULT,
+    DUE_DATE_ASCENDING,
+    DUE_DATE_DESCENDING,
+}
